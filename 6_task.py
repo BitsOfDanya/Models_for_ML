bb8cfec6be6581bf383823487bef1388ee53e251
Chepko Daniil <83452101+BitsOfDanya@users.noreply.github.com>
2023-12-16 22:46:11 +0300

    new result
    13 on 6 task
    8 on 6 task

--- 6_task.py
+++ 6_task.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import class_weight
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, LeakyReLU, BatchNormalization
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 
 train_data_path = 'data_real_train.csv'
@@ -19,48 +19,53 @@ X = train_data.drop('class', axis=1)
 y = train_data['class']
 X_test = test_data
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_val_scaled = scaler.transform(X_val)
+X_scaled = scaler.fit_transform(X)
 X_test_scaled = scaler.transform(X_test)
 
-class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
-class_weights = dict(enumerate(class_weights))
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-model = Sequential([
-    Dense(512, input_dim=X_train_scaled.shape[1], activation='relu', kernel_regularizer='l2'),
-    Dropout(0.5),
-    Dense(256, activation='relu', kernel_regularizer='l2'),
-    Dropout(0.5),
-    Dense(128, activation='relu', kernel_regularizer='l2'),
-    Dropout(0.5),
-    Dense(1, activation='sigmoid')
-])
+for train_index, val_index in kf.split(X_scaled):
+    X_train, X_val = X_scaled[train_index], X_scaled[val_index]
+    y_train, y_val = y[train_index], y[val_index]
 
-model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
+    class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+    class_weights = dict(enumerate(class_weights))
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+    model = Sequential([
+        Dense(1024, input_dim=X_train.shape[1], kernel_regularizer='l2'),
+        LeakyReLU(alpha=0.01),
+        BatchNormalization(),
+        Dropout(0.5),
+        Dense(512, kernel_regularizer='l2'),
+        LeakyReLU(alpha=0.01),
+        BatchNormalization(),
+        Dropout(0.5),
+        Dense(1, activation='sigmoid')
+    ])
 
-history = model.fit(X_train_scaled, y_train, epochs=100, batch_size=128, class_weight=class_weights,
-                    validation_data=(X_val_scaled, y_val), callbacks=[early_stopping], verbose=1)
+    model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=50, restore_best_weights=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, min_lr=0.00001, verbose=1)
+
+    history = model.fit(X_train, y_train, epochs=200, batch_size=128, class_weight=class_weights,
+                        validation_data=(X_val, y_val), callbacks=[early_stopping, reduce_lr], verbose=1)
+
+    print("\nTraining History:")
+    for epoch, accuracy, loss in zip(range(len(history.history['accuracy'])), history.history['accuracy'], history.history['loss']):
+        print(f"Epoch {epoch + 1} - Accuracy: {accuracy:.4f}, Loss: {loss:.4f}")
+
+    y_val_pred = model.predict(X_val)
+    y_val_pred = [1 if x > 0.5 else 0 for x in y_val_pred]
+    print("\nClassification Report on Validation Data:")
+    print(classification_report(y_val, y_val_pred))
+    print("Confusion Matrix on Validation Data:")
+    print(confusion_matrix(y_val, y_val_pred))
+    print("ROC AUC Score on Validation Data:")
+    print(roc_auc_score(y_val, y_val_pred))
 
 test_predictions = model.predict(X_test_scaled)
 test_predictions = [1 if x > 0.5 else 0 for x in test_predictions]
 output = ''.join(map(str, test_predictions))
-
-print("Прогноз на тестовых данных:", output)
-print("\nClassification Report on Training Data:")
-y_pred_train = model.predict(X_train_scaled)
-y_pred_train = [1 if x > 0.5 else 0 for x in y_pred_train]
-print(classification_report(y_train, y_pred_train))
-
-print("\nTraining History:")
-for epoch, accuracy, loss in zip(range(len(history.history['accuracy'])), history.history['accuracy'], history.history['loss']):
-    print(f"Epoch {epoch + 1} - Accuracy: {accuracy:.4f}, Loss: {loss:.4f}")
-
-print("\nConfusion Matrix on Training Data:")
-print(confusion_matrix(y_train, y_pred_train))
-print("\nROC AUC Score on Training Data:")
-print(roc_auc_score(y_train, y_pred_train))
+print("\nПрогноз на тестовых данных:", output)
